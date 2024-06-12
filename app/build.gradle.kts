f69f6d6b23dd3e8147ed7409f741421145f3b3da
zeorns <ljg5846@gmail.com>
2024-06-12 22:07:47 +0900

    feat: LP-1 minSDK 24 -> 27

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "kr.sdbk.lifeplanner"
-        minSdk = 24
+        minSdk = 27
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
