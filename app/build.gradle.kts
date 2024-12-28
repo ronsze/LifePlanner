5d9b84e7b5a9fcbfa4c0b4e0c883c54f50215b0e
zerons <ljg5846@gmail.com>
2024-12-29 07:58:51 +0900

    app add hilt dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
     alias(libs.plugins.kotlinx.serialization)
 }
 
@@ -61,5 +63,7 @@ dependencies {
 
     implementation(platform(libs.compose.bom))
     implementation(libs.bundles.compose)
+    implementation(libs.hilt)
+    ksp(libs.hilt.compiler)
     implementation(libs.kotlinx.serialization)
 }
