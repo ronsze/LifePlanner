1f3e3c2c8a509313fa2cbeafa7ff0f03ff36a177
zerons <ljg5846@gmail.com>
2025-01-20 16:15:39 +0900

    feature-diary feature : date picker 추가

--- core/common/src/main/java/kr/sdbk/common/Functions.kt
+++ core/common/src/main/java/kr/sdbk/common/Functions.kt
@@ -2,6 +2,32 @@ package kr.sdbk.common
 
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import java.text.SimpleDateFormat
+import java.time.format.DateTimeFormatter
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 inline fun <reified T> Json.encodeToStringOrEmpty(value: T?): String = if (value == null) "" else Json.encodeToString(value)
 inline fun <reified T> Json.decodeFromStringOrNull(value: String): T? = if (value == "") null else Json.decodeFromString<T>(value)
+
+object TimeFunctions {
+    private val defaultDateFormat = "yyyy-MM-dd"
+    private const val DAY_MILLIS = 86400000
+
+    fun getToday() = convertMillisToDate(System.currentTimeMillis())
+
+    fun convertMillisToDate(millis: Long): String {
+        val sdf = SimpleDateFormat(defaultDateFormat, Locale.getDefault())
+        return sdf.format(Date(millis))
+    }
+
+    fun convertDateToMillis(dateString: String): Long {
+        val sdf = SimpleDateFormat(defaultDateFormat, Locale.getDefault())
+        val date = sdf.parse(dateString)
+        return date!!.time
+    }
+
+    fun getNextDay(dateString: String) = convertMillisToDate(convertDateToMillis(dateString) + DAY_MILLIS)
+    fun getPreviousDay(dateString: String) = convertMillisToDate(convertDateToMillis(dateString) - DAY_MILLIS)
+}
--- feature/schedule/src/main/java/kr/sdbk/schedule/ScheduleView.kt
+++ feature/schedule/src/main/java/kr/sdbk/schedule/ScheduleView.kt
@@ -1,6 +1,5 @@
 package kr.sdbk.schedule
 
-import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -21,23 +20,16 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults
-import androidx.compose.material3.pulltorefresh.pullToRefresh
-import androidx.compose.material3.pulltorefresh.pullToRefreshIndicator
 import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
